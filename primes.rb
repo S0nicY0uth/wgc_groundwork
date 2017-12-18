c71568f291efacfb8471a67a0bbd2613f65596d8
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2017-12-18 00:36:54 +0000

    added prime solution

--- primes.rb
+++ primes.rb
@@ -1,6 +1,7 @@
+require 'prime'
+
 class Primes
   def self.sum_to(limit = 100)
-    # TODO - add your prime number solution here...
-    "I'm working on it!"
+    Prime.each(limit).inject(&:+)
   end
 end
